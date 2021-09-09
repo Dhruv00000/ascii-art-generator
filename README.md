a411d0bc4e091bbcb055ae4cc9c4583f84c8c470
DhruvMitna19 <dhruvmitna9@gmail.com>
2021-09-09 09:24:57 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# How to use
+
+1. Install [python](https://python.org/downloads).
+2. Download [`ascii.py`](https://github.com/dhruvmitna/ascii-art/blob/master/ascii.py).
+3. Place the image you want to convert in the downloads folder.
+4. Press `win + r`.
+5. Type `'cmd'` and press enter.
+6. Type `'cd Downloads'` and press enter.
+7. Type `'ascii.py'` and press enter.
+8. Open `ascii_image.txt` for the ascii art.
---
+++ ascii.py
@@ -0,0 +1,22 @@
+from PIL import Image
+
+def resize_image(image, new_width=100):
+
+    width, height = image.size
+    return image.resize((new_width, int(new_width * height / width)))
+
+def grayify(image):
+
+    return image.convert("L")
+
+def pixels_to_ascii(image):
+
+    return "".join(["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."] [pixel//25] for pixel in image.getdata())
+
+print("\n".join(pixels_to_ascii(grayify(resize_image(Image.open("image.png"))))[index : (index + 100)] for index in range(0, len(pixels_to_ascii(grayify(resize_image(Image.open("image.png"))))), 100)))
+
+with open("ascii_image.txt", "w") as f:
+    f.write("\n".join(pixels_to_ascii(grayify(resize_image(Image.open("image.png"))))[index : (index + 100)] for index in range(0, len(pixels_to_ascii(grayify(resize_image(Image.open("image.png"))))), 100)))
+
+with open("ascii_image.txt", "w") as f:
+    f.write("\n".join(pixels_to_ascii(grayify(resize_image(Image.open("image.png"))))[index : (index + 100)] for index in range(0, len(pixels_to_ascii(grayify(resize_image(Image.open("image.png"))))), 100)))
